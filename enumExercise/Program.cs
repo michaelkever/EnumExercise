81dee9eaa41c048e1ed6960d8659607d196e1afc
Michael Kever <michaelkever@gmail.com>
2018-07-21 16:33:33 -0500

    Initial commit

---
+++ enumExercise/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumExercise
+{
+    class Program
+    {
+        public enum daysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the current day of the week. \n Use correct format then press Enter:");
+            string userInput = Console.ReadLine();
+            daysOfTheWeek days = (daysOfTheWeek)Enum.ToObject(typeof(daysOfTheWeek), 7);
+
+            try
+            {
+                days = (daysOfTheWeek)Enum.Parse(typeof(daysOfTheWeek), userInput);
+                {
+                    Console.WriteLine("You entered: " + days);
+                }
+               
+            }
+            catch (Exception)
+                {
+                Console.WriteLine("Please enter an actual day of the week. (Capitalize correctly)");
+                }
+            
+
+            Console.ReadLine();
+
+        }
+        
+
+    }
+}
+
+
+
